41aec638994e554bd43d175bc3d8aa1af0c2766b
TaurusA <realtaurus97@gmail.com>
2024-01-27 14:48:37 -0600

    Select Statements revised

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,32 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            {
+                var r = new Random();
+                var favNumber = r.Next(1, 1000);
+
+                Console.WriteLine("My favorite number is between 1 and 1000!!!!!!!!");
+
+                while (true)
+                {
+                    var userInput = int.Parse(Console.ReadLine());
+
+                    if (userInput < favNumber)
+                    {
+                        Console.WriteLine("Almost there but too low!!!!!!");
+                    }
+                    else if (userInput > favNumber)
+                    {
+                        Console.WriteLine("Almost there but too high.");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"My favorite number is {favNumber}, thanks for playing.");
+                        break;
+                    }
+                }
+            }
+
         }
     }
 }
